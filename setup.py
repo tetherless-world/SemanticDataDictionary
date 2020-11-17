8cb3a3e71642d2d00bd29ab2aaca3363af7849ff
Jim McCusker <mccusker@gmail.com>
2020-11-17 15:21:23 -0500

    Packaged sdd2rdf.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "sdd2rdf",
+    version = "0.1",
+    author = "Jim McCusker",
+    author_email = "mccusj@cs.rpi.edu",
+    description = ("sdd2rdf generates RDF graphs from semantically annotated data."),
+    license = "Apache License 2.0",
+    keywords = "rdf semantic etl",
+    url = "http://packages.python.org/sdd2rdf",
+    packages=['sdd2rdf'],
+    long_description='''SETLr is a tool for generating RDF graphs, including named graphs, from almost any kind of tabular data.''',
+    include_package_data = True,
+    install_requires = [
+        'future',
+        'rdflib',
+        'rdflib-jsonld',
+        'pandas>=0.23.0',
+    ],
+    entry_points = {
+        'console_scripts': ['sdd2rdf=sdd2rdf:main'],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
