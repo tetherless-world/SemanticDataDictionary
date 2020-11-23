003637c18b27adc48e3fa7f0e23d5283cdb7bc2b
Jim McCusker <mccusker@gmail.com>
2020-11-23 18:16:07 -0500

    Everything should be working except the timeline start/end stuff.

--- sdd2rdf/sdd2setl.py
+++ sdd2rdf/sdd2setl.py
@@ -5,26 +5,100 @@ import pandas as pd
 import argparse
 import numpy as np
 import re
+from setlr import isempty
 from slugify import slugify
 
 base_context = {
+    "sio": "http://semanticscience.org/resource/",
+    "owl": "http://www.w3.org/2002/07/owl#",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+    "prov": "http://www.w3.org/ns/prov#",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
     "Attribute" : { "@id" : "rdf:type", "@type" : "@id"},
     "Entity" : { "@id" : "rdf:type", "@type" : "@id"},
     "attributeOf" : { "@id" : "sio:isAttributeOf", "@type" : "@id"},
+    "Entity" : { "@id" : "rdf:type", "@type" : "@id"},
+    "inRelationTo" : { "@id" : "sio:inRelationTo", "@type" : "@id"},
+    "Role" : { "@id" : "sio:hasRole", "@type" : "@id"},
+    "Time" : { "@id" : "sio:existsAt", "@type" : "@id"},
+    "Unit" : { "@id" : "sio:hasUnit", "@type" : "@id"},
+    "Value" : { "@id" : "sio:hasValue"},
+    "wasDerivedFrom" : { "@id" : "prov:wasDerivedFrom", "@type" : "@id"},
+    "wasGeneratedBy" : { "@id" : "prov:wasGeneratedBy", "@type" : "@id"},
 }
 
 class SemanticDataDictionary:
-    _columns = None
-    _codebook = None
-    _resource_codebook = None
-    _context = None
-    _codemap = None
-    _timeline = None
-    _infosheet = None
+    columns = None
+    codebook = None
+    resource_codebook = None
+    context = None
+    codemap = None
+    timeline = None
+    infosheet = None
 
-    def __init(self, sdd_path, prefix):
+    def __init__(self, sdd_path, prefix):
         self.sdd_path = sdd_path
         self.prefix = prefix
+        self.load()
+
+    def load(self):
+        infosheet = self._get_table()
+        self.infosheet = dict([(row.Attribute, row.Value)
+                               for i, row in infosheet.iterrows()
+                               if not isempty(row.Value)])
+
+        codemap = self._get_table('Code Mapping')
+
+        self.codemap = dict([(row.code, row.uri)
+                               for i, row in codemap.iterrows()
+                               if not isempty(row.uri)])
+
+        codebook = self._get_table('Codebook')
+        self.codebook = dict([((row.Column, row.Code),
+                                self._split_and_map(row.Class))
+                               for i, row in codebook.iterrows()
+                               if not isempty(row.Class)])
+        self.resource_codebook = dict([((row.Column, row.Code),
+                                         self._split_and_map(row.Resource))
+                                        for i, row in codebook.iterrows()
+                                        if not isempty(row.Resource)])
+
+        self.context = {}
+        self.context.update(base_context)
+        self.context['@base'] = self.prefix
+        if 'Prefixes' in self.infosheet:
+            prefixes = self._get_table('Prefixes')
+            self.context.update(dict([(str(row.prefix), row.url)
+                                       for i, row in prefixes.iterrows()
+                                       if not isempty(row.prefix) and not isempty(row.url)]))
+
+        dm = self._get_table('Dictionary Mapping')
+        self.columns = dict([(col.Column, col.to_dict()) for i, col in dm.iterrows()])
+        timeline = self._get_table('Timeline')
+        for i, t in timeline.iterrows():
+            if t.Name in self.columns:
+                self.columns[t.Name].update(t.to_dict())
+            else:
+                self.columns[t.Name] = t.to_dict()
+                self.columns[t.Name]['Column'] = t.Name
+        self.column_templates = {}
+        for key, col in self.columns.items():
+            for annotation in ['Unit','Format','Role','Relation']:
+                if annotation in col and not isempty(col[annotation]):
+                    col[annotation] = self.codemap.get(col[annotation],col[annotation])
+            for annotation in ['wasDerivedFrom','wasGeneratedBy']:
+                if annotation in col and not isempty(col[annotation]):
+                    col[annotation] = self._split(col[annotation])
+            for annotation in ['Attribute','Entity','Type']:
+                if annotation in col and not isempty(col[annotation]):
+                    col[annotation] = self._split_and_map(col[annotation])
+            self.column_templates[col['Column']] = "{{row.get('%s')}}"%col['Column']
+        for col in self.columns.values():
+            template = slugify(col['Column'])+'-{i}'
+            template  =col.get('Template',template)
+            col['uri_template'] = template.format(i='{{i}}',**self.column_templates)
+
 
     def _get_table(self, entry=None):
         if entry is None:
@@ -33,98 +107,31 @@ class SemanticDataDictionary:
             path = self.infosheet[entry]
         if '#' in path:
             infosheet_path, sheetname  = self.sdd_path.split('#')
-            sheetname = path.replace('#','',1)
-            return pd.read_excel(infosheet_path, sheetname=sheetname)
+            if path != self.sdd_path:
+                sheetname = path.replace('#','',1)
+            return pd.read_excel(infosheet_path, sheet_name=sheetname)
         else:
             return pd.read_csv(path)
 
     def _split(self, value):
+        if isempty(value):
+            return []
         return re.split('\\\\s*[,;&]\\\\s*', value)
 
     def _split_and_map(self, value):
+        if isempty(value):
+            return []
         return [self.codemap.get(x,x) for x in self._split(value)]
-
-    @property
-    def infosheet(self):
-        if self._infosheet is  None:
-            infosheet = self._get_table()
-            self._infosheet = dict([(row.Attribute, row.Value)
-                                   for i, row in infosheet.iterrows()
-                                   if not np.is_nan(row.Value)])
-        return self._infosheet
-
-    @property
-    def codemap(self):
-        if self._codemap is None:
-            codemap = self._get_table('Code Mapping')
-            self._codemap = dict([(row.code, row.uri)
-                                   for i, row in codemap.iterrows()
-                                   if not np.is_nan(row.uri)])
-        return self._codemap
-
-    @property
-    def codebook(self):
-        if self._codebook is None:
-            codebook = self._get_table('Codebook')
-            self._codebook = dict([((row.Column, row.Code),
-                                    self._split_and_map(row.Class))
-                                   for i, row in codebook.iterrows()
-                                   if not np.is_nan(row.Class)])
-        return self._codebook
-
-    @property
-    def resource_codebook(self):
-        if self._resource_codebook is None:
-            codebook = self._get_table('Codebook')
-            self._resource_codebook = dict([((row.Column, row.Code),
-                                             self._split_and_map(row.Resource))
-                                            for i, row in codebook.iterrows()
-                                            if not np.is_nan(row.Resource)])
-        return self._resource_codebook
-
-    @property
-    def context(self):
-        if self._context is None:
-            self._context = {}
-            self._context.update(base_context)
-            self._context['@base'] = self.prefix
-            if 'Prefixes' in self.infosheet:
-                codebook = self._get_table('Prefixes')
-                self._context.update(dict([(row.code, row.uri)
-                                           for i, row in codebook.iterrows()]))
-        return self._context
-
-    @property
-    def columns(self):
-        if self._columns is None:
-            dm = self._get_table('Dictionary Mapping')
-            self._columns = {}
-            self._column_templates = {}
-            for i, column in dm.iterrows():
-                col = column.as_dict()
-                for annotation in ['Unit','Format','Role']:
-                    if annotation in col and not np.is_nan(col[annotation]):
-                        col[annotation] = self._split(col[annotation])
-                for annotation in ['inRelationTo','wasDerivedFrom','wasGeneratedBy']:
-                    if annotation in col and not np.is_nan(col[annotation]):
-                        col[annotation] = self._split(col[annotation])
-                for annotation in ['Attribute','Entity','Role']:
-                    if annotation in col and not np.is_nan(col[annotation]):
-                        col[annotation] = self._split_and_map(col[annotation])
-                self._column_templates[col['Column']] = "{{row.get('%s')}}"%col['Column']
-                self._columns[col['Column']] = col
-            for col in self._columns.values():
-                template = slugify(col['Column'])+'-{i}'
-                template  =col.get('Template',template)
-                col['uri_template'] = template.format(i='{{i}}'**self._column_templates)
-        return self._columns
-
 
 def sdd2setl(semantic_data_dictionary, prefix, datafile, **kwargs):
     sdd = SemanticDataDictionary(semantic_data_dictionary,prefix)
     env = Environment(loader=PackageLoader('sdd2rdf', 'templates'))
     template = env.get_template('sdd_setl_template.jinja')
-    output = template.render(sdd=sdd, prefix=prefix, data=datafile)
+    output = template.render(sdd=sdd,
+                             prefix=prefix,
+                             data=datafile,
+                             str=str,
+                             isempty=isempty)
     return output
 
 def sdd2setl_main():
@@ -135,7 +142,6 @@ def sdd2setl_main():
     parser.add_argument("output_file")
 
     args = parser.parse_args()
-    output = sdd2setl(args['semantic_data_dictionary'],
-                      args['prefix'], args['datafile'])
-    with open(args['output_file'], 'wb') as o:
+    output = sdd2setl(args.semantic_data_dictionary, args.prefix, args.data_file)
+    with open(args.output_file, 'wb') as o:
         o.write(output.encode('utf8'))
