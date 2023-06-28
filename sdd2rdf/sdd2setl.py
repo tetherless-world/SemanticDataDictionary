e09e231b4f21aa0883e3d6689f76e18f59d6bb7f
Jamie McCusker <mccusj2@rpi.edu>
2023-06-28 11:18:10 -0400

    Added support for complex nested XML data.

--- sdd2rdf/sdd2setl.py
+++ sdd2rdf/sdd2setl.py
@@ -9,6 +9,8 @@ from setlr import isempty
 from slugify import slugify
 import io
 import puremagic
+import json
+import fnmatch
 
 base_context = {
     "void" : "http://rdfs.org/ns/void#",
@@ -34,7 +36,10 @@ base_context = {
     "TimeInterval" : {"@id" : "sio:TimeInterval"},
     "wasDerivedFrom" : { "@id" : "prov:wasDerivedFrom", "@type" : "@id"},
     "wasGeneratedBy" : { "@id" : "prov:wasGeneratedBy", "@type" : "@id"},
+    "a" : {"@id": "rdf:type"}
 }
+
+
 
 class SemanticDataDictionary:
     columns = None
@@ -47,11 +52,28 @@ class SemanticDataDictionary:
     sdd_format = None
     sheets = None
     data = None
+    data_type = None
 
-    def __init__(self, sdd_path, prefix):
+    def __init__(self, sdd_path, prefix, data_type):
         self.sdd_path = sdd_path
         self.prefix = prefix
+        self.data_type = data_type
         self.load()
+
+    def _expand_codebook(self):
+        codebook = dict(self.codebook)
+        for key, value in self.codebook.items():
+            for column in self.columns.keys():
+                if key[0] != column and fnmatch.fnmatch(column, key[0]):
+                    codebook[(column,key[1])] = value
+        self.codebook = codebook
+
+        resource_codebook = dict(self.resource_codebook)
+        for key, value in self.resource_codebook.items():
+            for column in self.columns.keys():
+                if key != column and fnmatch.fnmatch(column, key):
+                    resource_codebook[column] = value
+        self.resource_codebook = resource_codebook
 
     def load(self):
         infosheet = self._get_table()
@@ -88,9 +110,11 @@ class SemanticDataDictionary:
             self.context.update(prefix_dict)
 
         dm = self._get_table('Dictionary Mapping')
-        self.columns = dict([(col.Column, col.to_dict())
+        self.columns = dict([(col.Column, col.dropna().to_dict())
                              for i, col in dm.iterrows()
                              if not isempty(col['Column'])])
+        for key, col in self.columns.items():
+            col['Column'] = col['Column'].strip()
         timeline = self._get_table('Timeline')
         for i, t in timeline.iterrows():
             if isempty(t.Name):
@@ -103,6 +127,8 @@ class SemanticDataDictionary:
         self.column_templates = {}
         self.value_templates = {}
         for key, col in self.columns.items():
+            col['children'] = []
+        for key, col in self.columns.items():
             for annotation in ['Unit','Format','Role','Relation']:
                 if annotation in col and not isempty(col[annotation]):
                     col[annotation] = self.codemap.get(col[annotation],col[annotation])
@@ -112,10 +138,24 @@ class SemanticDataDictionary:
             for annotation in ['Attribute','Entity','Type']:
                 if annotation in col and not isempty(col[annotation]):
                     col[annotation] = self._split_and_map(col[annotation])
-            self.value_templates[slugify(col['Column'],separator="_")] = "{{row.get('%s')}}"%col['Column']
-            #self.column_templates[slugify(col['Column'],separator="_")] = "{{row.get('%s').replace(' ','_').replace('>','').replace('\\','')}}"%col['Column']
-            self.column_templates[slugify(col['Column'],separator="_")] = "{{slugify(str(row.get('%s')),separator='_',lowercase=False)}}"%col['Column']
-            if not col['Column'].startswith('??'):
+            et = element_templates[self.data_type]
+            column_id = slugify(col['Column'],separator="_")
+            parent = col.get('SelectorParent','__tree_root__')
+            if parent != '__tree_root__':
+                self.columns[parent]['children'].append(col)
+                col['parent_column'] = self.columns[parent]
+            selector = col.get('Selector', None)
+#            if parent is None:
+#                col['iterator'] = '[row]'
+#            else:
+            if 'Selector' in col:
+                col['iterator'] = et['iterate_template']%(parent, selector)
+            else:
+                col['iterator'] = et['iterate_template']
+            self.value_templates[column_id] = et['value_template']%col['Column']
+            self.column_templates[column_id] = et['column_template']%col['Column']
+            col['value_access'] = et['value'] % col['Column']
+            if not col['Column'].startswith('??') and not col['Column'].startswith('__'):
                 col['@value'] = '{%s}'%slugify(col['Column'],separator="_")
             if 'Format' in col and not isempty(col['Format']):
                 value_type = col['Format'].split("^^")
@@ -125,15 +165,60 @@ class SemanticDataDictionary:
                     col['@type'] = value_type[1]
                     if len(value_type[0]) > 0:
                         col['@value'] = value_type[0]
+        self.root_columns = [c for c in self.columns.values() if 'SelectorParent' not in c]
+        self.root_columns = [{
+            'Column': '__tree_root__',
+            'iterator': '[row]',
+            'children': self.root_columns
+        }]
+        for col in self.root_columns + list(self.columns.values()):
+            # create a @with statement to set the local scope for this subtree.
+            if len(col['children']) > 0:
+                col['leaves'] = []
+                col['branches'] = []
+                assignments = []
+                variables = []
+                for child in col['children']:
+                    if len(child['children']) > 0:
+                        col['branches'].append(child)
+                    else:
+                        col['leaves'].append(child)
+                        if not child['Column'].startswith('??'):
+                            variables.append("%s" % child['Column'])
+                            assignments.append('%s[0] if len(%s) > 0 else None, ' % (child['iterator'].replace('\\','\\\\'), child['iterator'].replace('\\','\\\\')))
+                col['with'] = ['('] + assignments + [') as ', ', '.join(variables)]
+            del col['children']
+                #print(col.get('Column',"root"))
+                #print(col['with'])
+                #print(col['leaves'])
+                #print(col['branches'])
+
+        #print(json.dumps(self.root_columns, indent=4))
+
+        self._expand_codebook()
+
         for col in self.columns.values():
             default = slugify(col['Column'],separator="_")+'_{i}'
-
             template = col.get('Template', default)
+            print(col['Column'], template)
             if isempty(template):
                 template = slugify(col['Column'],separator="_")+'_{i}'
 
             template = re.sub(r'(:<=\{).*(:=\})',lambda x:str(slugify(x.group(0),separator="_")),template)
-            col['uri_template'] = template.format(i='{{name}}',**self.column_templates)
+            formatted_template = template.format(i='{{name}}',**self.column_templates)
+            if col['Column'] in self.resource_codebook:
+                formatted_template = ''.join([
+                    "{% if '",
+                    col['Column'],
+                    "' in resource_codebook %}{{resource_codebook['",
+                    col['Column'],
+                    "'][",
+                    col['value_access'],
+                    "]}}{% else %}",
+                    formatted_template,
+                    "{% endif %}"])
+            col['uri_template'] = formatted_template
+
             if '@value' in col:
                 col['@value'] = col['@value'].format(i='{{name}}',**self.value_templates)
 
@@ -191,9 +276,34 @@ class SemanticDataDictionary:
             return []
         return [self.codemap.get(x,x) for x in self._split(value)]
 
+element_templates = {
+    'setl:XML': {
+        # How are we going to handle nested iteration?
+        'value_template' : "{{%s}}",
+        'value' : "%s",
+        'iterate_template' : "%s.xpath('%s')",
+        'column_template' : "{{slugify(str(%s),separator='_',lowercase=False)}}"
+    },
+    "setl:Excel" : {
+        'value_template' : "{{row.get('%s')}}",
+        'value' : "row.get('%s')",
+        'iterate_template' : "[row]",
+        'column_template' : "{{slugify(str(row.get('%s')),separator='_',lowercase=False)}}"
+    },
+    "csvw:Table" : {
+        'value_template' : "{{row.get('%s')}}",
+        'value' : "row.get('%s')",
+        'iterate_template' : "[row]",
+        'column_template' : "{{slugify(str(row.get('%s')),separator='_',lowercase=False)}}"
+    }
+}
+
 file_types = {
     "text/csv" : "csvw:Table",
     "csv" : "csvw:Table",
+    "application/xml" : 'setl:XML',
+    'text/xml': 'setl:XML',
+    'xml' : 'setl:XML',
     "excel" : "setl:Excel",
     'application/vnd.ms-excel': "setl:Excel",
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': "setl:Excel",
@@ -203,7 +313,9 @@ def sdd2setl(semantic_data_dictionary, prefix, datafile,
              format='csv', delimiter=',', sheetname=None, output=None, dataset_uri=None):
     if dataset_uri is None:
         dataset_uri = prefix+'dataset'
-    sdd = SemanticDataDictionary(semantic_data_dictionary,prefix)
+    if format != 'csv':
+        delimiter = None
+    sdd = SemanticDataDictionary(semantic_data_dictionary,prefix, data_type=file_types[format])
     env = Environment(loader=PackageLoader('sdd2rdf', 'templates'))
     template = env.get_template('sdd_setl_template.jinja')
     output = template.render(sdd=sdd,
@@ -225,7 +337,7 @@ def sdd2setl_main():
     parser.add_argument("data_file")
     parser.add_argument("setl_output")
     parser.add_argument('-o', "--output")
-    parser.add_argument('-f', "--format",default='csv', choices=['csv','excel'])
+    parser.add_argument('-f', "--format",default='csv', choices=['csv','excel','xml'])
     parser.add_argument('-d', "--delimiter", default=',')
     parser.add_argument('-s', '--sheetname')
     parser.add_argument("--dataset_uri")
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "sdd2rdf",
-    version = "1.0.4",
+    version = "1.0.5",
     author = "Jamie McCusker",
     author_email = "mccusj@cs.rpi.edu",
     description = ("sdd2rdf generates RDF graphs from semantically annotated data."),
